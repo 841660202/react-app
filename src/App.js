92b511b6bb4b11e26dadb729745bbdef5612577a
chenhailong <841660202@qq.com>
2017-07-27 16:05:36 +0800

    change directory structure

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@
 import React, {Component} from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import DangerButton from './DangerButton';
 import {hashHistory} from 'react-router';
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
 
@@ -13,13 +12,13 @@ import SideBar from './containers/Home/SideBar';
 import AnimationRouter from './containers/Home/AnimationRouter';
 
 
-import AntButton from './containers/Ant/Ant';
+import Ant from './containers/Ant/Ant';
 class App extends Component {
   render() {
     return (
 
       <div style={{height:'100%',width:'100%'}}>
-        <AntButton></AntButton>
+        <Ant></Ant>
         {/*<h2 className="red">嵌套路由</h2>
 
         <RouteMap/>
